dace0976276d062e43293d31722188bd8eac3154
sach-dev <sachinm0@chicagobooth.edu>
2020-10-17 01:15:23 +0000

    finished project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,34 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+  
   def play_rock 
 
     #redirect_to("https://www.wikipedia.com")
     #render({ :html => "<h1>Hello, World!</h1>".html_safe })
     render({ :template => "game_templates/user_rock.html.erb"})
   end
+
+  def play_paper
+    
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock" 
+      @outcome = "won"
+    elsif @comp_move == "paper" 
+      @outcome = "tied"
+    elsif @comp_move == "scissors" 
+      @outcome = "lost"
+    end
+    
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end
+  
+  def play_scissors
+    render({ :template => "game_templates/user_scissors.html.erb"})
+  end
+  
 end
